7ec467b501ad0f263960074fbf62dd20221612ca
paloma.souza <paloma.souza@ambevtech.com.br>
2022-06-12 17:03:10 -0300

    first commit

---
+++ aprendizados.md
@@ -0,0 +1,4 @@
+yarn add firebase   
+
+https://firebase.google.com/docs/auth/web/google-signin
+https://firebase.google.com/docs/database/web/start
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './services/firebase'
+
+function App() {
+  return (
+   <>
+   <h1>Let me ask</h1>
+   </>
+  );
+}
+
+export default App;
